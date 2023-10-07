6c86277c6feab9f5146ecee1e30ae9bcf936aa03
antoine <21904654@unicaen.fr>
2023-10-07 19:22:19 +0200

    pdf cracker

---
+++ pdf_cracker/cracker.py
@@ -0,0 +1,22 @@
+# erreur lors du chargement de rockyou.txt à la lecture du byte 0xf1
+# tfaçon python c'est guez donc on va faire ça en rust
+
+import pikepdf, argparse, time
+
+parser = argparse.ArgumentParser(description="PDF Password Cracker")
+parser.add_argument("-f", "--file", dest="pdfile", help="PDF file to crack", default="CV_Antoine_psswd.pdf")
+parser.add_argument("-w", "--wordlist", dest="psswdlist", help="Password list", default="passwd.txt")
+
+def cracker(pdfile, psswdlist) -> str:
+    for password in [i.strip() for i in  open(psswdlist)]:
+        try:
+            with pikepdf.open(pdfile, password=password) as pdf:
+                return password
+        except pikepdf._core.PasswordError as e:
+            continue
+    return "Password not found"
+
+start = time.time()
+print(cracker(parser.parse_args().pdfile, parser.parse_args().psswdlist))
+end = time.time()
+print(f"Time taken: {end-start} seconds")
---
+++ pdf_cracker_rust/src/main.rs
@@ -0,0 +1,74 @@
+/*
+    Implémentation en Rust
+    ======================
+    voilà ça c'est pas un language imprévisible
+        -> lecture des bytes sans problème
+        -> pas ralenti par le garbage collector
+        -> pas de problème de type
+        -> pas de problème de compilation
+        -> merci le mec qui a invneité le rust en fait
+*/
+
+use std::fs::File as FsFile;
+use std::io::{BufRead, BufReader};
+use std::env;
+use std::time::Instant;
+
+use pdf;
+
+fn try_password(pdf_contents: &[u8], password: &str) -> bool {
+    pdf::file::FileOptions::cached()
+        .password(password.as_bytes())
+        .load(pdf_contents)
+        .is_ok()
+}
+
+
+fn main() {
+
+    println!("usage:\n\tcargo run <wordlist> <pdffile>");
+
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+
+    println!("reading: {}", filename);
+    let mut lines = Vec::new();
+
+    if let Ok(file) = FsFile::open(filename) {
+        // petit timer pour mesurer le temps d'exécution
+        let start_time = Instant::now();
+
+        // Créer un BufReader pour faciliter la lecture ligne par ligne
+        let reader = BufReader::new(file);
+
+        // Parcourir chaque ligne du fichier
+        for line in reader.lines() {
+            if let Ok(line_content) = line {
+                lines.push(line_content);
+            }
+        }
+        let elapsed_time = start_time.elapsed();
+
+        println!("{} words loaded in {}.{} seconds", lines.len(), elapsed_time.as_secs(), elapsed_time.subsec_millis());
+    } else {
+        println!("ERROR: can't read file.");
+    }
+
+    println!("now loading: {}", args[2]);
+    // Ouvrir le fichier en écriture
+    let pdf_bytes = std::fs::read(&args[2]).expect("Unable to read PDF");
+
+    println!("début du bruteforce.");
+    let start_time = Instant::now();
+
+    for potential_password in lines {;
+        if try_password(&pdf_bytes, &potential_password) {
+            let elapsed_time = start_time.elapsed();
+            println!("password found in {}.{} seconds: {}", elapsed_time.as_secs(), elapsed_time.subsec_millis(), &potential_password);
+            break;
+        }
+    }
+
+
+    
+}
