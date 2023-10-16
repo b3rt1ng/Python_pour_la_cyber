15422f5dec62f1f402628f545597131b69c82793
antoine <21904654@unicaen.fr>
2023-10-16 09:05:31 +0200

    pullup

--- zipfile_cracker/zipcrack.py
+++ zipfile_cracker/zipcrack.py
@@ -5,33 +5,36 @@
 #expérimentations update:   j'aimerais tester et voir combien de mdp marchent avec une grosse wordlist (rockyou.txt).
 #                           on a quand même environ 0.4% des mdp qui fonctionnent (chokbar)
 
-import zipfile, sys
+import zipfile, sys, argparse
+
+parser = argparse.ArgumentParser(description="Crack a zip file")
+parser.add_argument("-f", "--file", help="Zip file to crack", required=True)
 
 colors = BLUE, RED, WHITE, YELLOW, MAGENTA, GREEN, END = '\033[94m', '\033[91m', '\033[0m', '\033[93m', '\033[95m', '\033[92m', '\033[0m'
 
 def crack_password(password_list, obj):
-    count = 0
-    total = 0
+    # count = 0
+    # total = 0
     with open(password_list, 'rb') as file:
         for line in file:
-            total += 1
+            # total += 1
             for word in line.split():
                 try:
                     obj.extractall(pwd=word)
-                    count += 1
+                    # count += 1
                     # percent = count * 100 / total
-                    # sys.stdout.write(f"Password found:{GREEN} {word.decode()}{WHITE} {round(percent, 2)}%\n")
+                    sys.stdout.write(f"Password found:{GREEN} {word.decode()}{WHITE}\n")
                     # input()
                     # return True
                 except:
-                    if total % 100000 == 0:
-                        percent = count * 100 / total
-                        sys.stdout.write(f"{round(percent, 2)}% of words in list are corresponding\n")
+                    # if total % 100000 == 0:
+                    #     percent = count * 100 / total
+                    #     sys.stdout.write(f"{round(percent, 2)}% of words in list are corresponding\n")
                     # print(f"Password {word.decode()} is not correct")
                     continue
     print("Password not found")
     return False
 
-zip = zipfile.ZipFile("test.zip")
+zip = zipfile.ZipFile(parser.parse_args().file)
  
 crack_password("/usr/share/wordlists/rockyou.txt", zip) 
